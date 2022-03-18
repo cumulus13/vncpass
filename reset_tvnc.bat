b88fe8ccbcbb9ab0345c7eb1bc3398d544667e33
dev01 <dev01@dev01.anyargroup.local>
2022-03-18 04:51:30 -0400

    version: 1.2.4 ~ 2022-03-18 04:51:28:480103

--- reset_tvnc.bat
+++ reset_tvnc.bat
@@ -2,8 +2,8 @@
 
 "C:\Program Files\TightVNC\tvnserver.exe" -stop
 "C:\Program Files (86)\TightVNC\tvnserver.exe" -stop
-reg add "HKLM\SOFTWARE\TightVNC\Server" /v Password /t REG_BINARY /d %1 /f
-reg add "HKLM\SOFTWARE\TightVNC\Server" /v ControlPassword /t REG_BINARY /d %1 /f
+reg add "HKLM\SOFTWARE\TightVNC\Server" /v Password /t REG_BINARY /d d9923c562a209db7 /f
+reg add "HKLM\SOFTWARE\TightVNC\Server" /v ControlPassword /t REG_BINARY /d d9923c562a209db7 /f
 reg add "HKLM\SOFTWARE\TightVNC\Server" /v UseVncAuthentication /t REG_DWORD /d 0x1 /f
 "C:\Program Files\TightVNC\tvnserver.exe" -start
 "C:\Program Files (86)\TightVNC\tvnserver.exe" -start
